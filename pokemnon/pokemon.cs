84bd3daad82e575d41294f0fa043e418655bb4de
Zdravko-jel <bg.nonickname@gmail.com>
2023-01-25 16:35:56 +0200

    Added new project

---
+++ pokemnon/pokemon.cs
@@ -0,0 +1,18 @@
+﻿int N = int.Parse(Console.ReadLine());
+int N1 = N;
+int M = int.Parse(Console.ReadLine());
+int Y = int.Parse(Console.ReadLine());
+int count = 0;
+
+while (N > M)
+{
+    N = N - M;
+    count++;
+    if (N == N1 / 2)
+    {
+        N = N / Y;
+    }
+}
+
+Console.WriteLine($"{N}");
+Console.WriteLine($"{count}");
